64c7b1bbe9c4d515d4a347095222b83a7e367d59
xubinzhe <243636623@qq.com>
2022-05-24 21:59:46 +0800

    修改test01.java

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -1,5 +1,7 @@
 public class Test01 {
     public static void main(String[] args){
         System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
     }
 }
